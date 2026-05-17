aee1e6d1cc20760a14dacb7080d58dbbb24b9522
A1VEN23 <120800456+A1VEN23@users.noreply.github.com>
2026-05-17 13:09:46 +0300

    fix: remove StrictMode to prevent double-effect black screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-// Deploy: Sun May 10 14:58:49 UTC 2026
+createRoot(document.getElementById('root')).render(<App />)
+// Deploy: Sun May 17 2026
